172760f65b6e02caec692edd9ec8c6edc66553e1
Drew Engelson <dsengelson@pbs.org>
2011-01-31 23:16:23 -0500

    Updated packaging info.

--- setup.py
+++ setup.py
@@ -3,12 +3,26 @@ from distutils.core import setup
 setup(
     name = 'django-simplegravatar',
     version = '0.2',
-    packages = ['simplegravatar',],
+    packages = ['simplegravatar', ],
+    package_data = {
+        'simplegravatar': [
+            'templates/templatetags/simplegravatar/*.html',
+            'templatetags/*.py'],
+    },
     author = 'Drew Engelson',
     author_email = 'drew@engelson.net',
     url = 'http://tomatohater.com/django-simplegravatar/',
-    license = 'Creative Commons Attribution-Noncommercial-Share Alike license',
-    summary = 'A simpler Django template tag to add Gravatar support to your Django projects.',
+    license = 'GPLv3',
+    description = 'A simpler Django template tag to add Gravatar support to your Django projects.',
     long_description = 'A simpler Django template tag to add Gravatar support to your Django projects.',
     keywords = 'django avatar gravatar',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GPLv3 License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
 )
