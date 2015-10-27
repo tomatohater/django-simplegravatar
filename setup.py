c0310c772950fcd3bcc9f6dfd9e17816501273e9
Drew Engelson <drew@engelson.net>
2015-10-26 21:25:56 -0400

    Bump version. Dang pypi!

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name = 'django-simplegravatar',
-    version = '0.3.1',
+    version = '0.3.2',
     packages = ['simplegravatar', ],
     package_data = {
         'simplegravatar': [
